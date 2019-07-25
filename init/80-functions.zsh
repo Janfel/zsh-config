74799f55bc8e47169dea9b63f1495df0f85e842f
Jan Felix Langenbach <o.hase3@gmail.com>
2019-07-25 22:04:20 +0200

    Add function comments

--- init/80-functions.zsh
+++ init/80-functions.zsh
@@ -1,11 +1,11 @@
-edit() { "${VISUAL:-${$EDITOR:-"/usr/bin/nano"}}" "$@" }
-alias ed=edit
-
+# Changes into a new directory
 mkcd() { mkdir -p "$@" && cd "$1" }
 
+# Recompiles the zsh configuration
 update_zsh() { $ZDOTDIR/zsh-cfg bundle compile }
 
+# Recompiles the zsh configuration and updates the cached packages
 upgrade_zsh() { update_zsh && antibody update }
 
+# Finds the package that owns a certain executable
 whose() { /usr/bin/yay --files --owns "/usr/bin/$@" }
-
