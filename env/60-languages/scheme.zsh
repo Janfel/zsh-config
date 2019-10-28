ee94b00b6e53e87f336ad4eabb7e8f294547d6d6
Jan Felix Langenbach <o.hase3@gmail.com>
2019-10-28 18:25:19 +0100

    Fix guile hist file

--- env/60-languages/scheme.zsh
+++ env/60-languages/scheme.zsh
@@ -4,11 +4,11 @@
 
 ## Guile Scheme
 
-export GUILE_HOME="$XDG_DATA_HOME/guile"
-export GUILE_HISTORY="$GUILE_HOME/history"
+#export GUILE_HOME="$XDG_DATA_HOME/guile"
+export GUILE_HISTORY="$RLWRAP_HOME/guile_history"
 
-typeset -xUT GUILE_LOAD_PATH guile_load_path
-guile_load_path=($GUILE_HOME/share $guile_load_path)
+#typeset -xUT GUILE_LOAD_PATH guile_load_path
+#guile_load_path=($GUILE_HOME/share $guile_load_path)
 
-typeset -xUT GUILE_LOAD_COMPILED_PATH guile_load_compiled_path
-guile_load_compiled_path=($GUILE_HOME/lib $guile_load_compiled_path)
+#typeset -xUT GUILE_LOAD_COMPILED_PATH guile_load_compiled_path
+#guile_load_compiled_path=($GUILE_HOME/lib $guile_load_compiled_path)
