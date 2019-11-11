1a6aabfc259cffcf83ac66ab7ae04611a7d69f5b
Jan Felix Langenbach <o.hase3@gmail.com>
2019-11-12 00:38:25 +0100

    Add XDG_USER_BASE

--- env/11-xdg-directories.zsh
+++ env/11-xdg-directories.zsh
@@ -1,3 +1,4 @@
-[[ -z $XDG_CONFIG_HOME ]] && export XDG_CONFIG_HOME="$HOME/.config"
-[[ -z $XDG_DATA_HOME ]] && export XDG_DATA_HOME="$HOME/.local/share"
-[[ -z $XDG_CACHE_HOME ]] && export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_USER_BASE="$HOME/.local"
+export XDG_DATA_HOME="$XDG_USER_BASE/share"
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_CACHE_HOME="$HOME/.cache"
