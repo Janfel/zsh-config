ed4ae98d6154c003971d56e6bc78c2b634ef8db6
Jan Felix Langenbach <o.hase3@gmail.com>
2019-11-12 00:36:43 +0100

    Add GIT_COMMAND

--- env/10-defaults.zsh
+++ env/10-defaults.zsh
@@ -1,5 +1,6 @@
 DEFAULT_USER="${DEFAULT_USER:-$USER}"
 
-export EDITOR="/usr/bin/micro"
-export VISUAL="/usr/bin/nvim"
-export PAGER="/usr/bin/less"
+export EDITOR='/usr/bin/micro'
+export VISUAL='/usr/bin/nvim'
+export PAGER='/usr/bin/less'
+export GIT_COMMAND='/usr/bin/git'
