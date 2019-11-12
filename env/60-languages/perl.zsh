c0a777956ecaf869c76a1f5f281f3749d8c191f3
Jan Felix Langenbach <o.hase3@gmail.com>
2019-11-12 02:00:05 +0100

    Perl CPAN modules in .local

--- env/60-languages/perl.zsh
+++ env/60-languages/perl.zsh
@@ -1,10 +1,16 @@
-export PERL_HOME="$XDG_DATA_HOME/perl5"
+
+# Package management
+
+export PERL_BASE="$XDG_USER_BASE"
 
 typeset -xUT PERL5LIB perl5lib
-perl5lib=($PERL_HOME $perl5lib)
+perl5lib=($PERL_BASE $perl5lib)
 
 typeset -xUT PERL_LOCAL_LIB_ROOT perl_local_lib_root
-perl_local_lib_root=($PERL_HOME $perl_local_lib_root)
+perl_local_lib_root=($PERL_BASE $perl_local_lib_root)
 
-export PERL_MB_OPT="--install_base \"$PERL_HOME\""
-export PERL_MM_OPT="INSTALL_BASE=\"$PERL_HOME\""
+typeset -xUT PERL_MB_OPT perl_mb_opt ' '
+perl_mb_opt+="--install_base '$PERL_BASE'"
+
+typeset -xUT PERL_MM_OPT perl_mm_opt ' '
+perl_mm_opt+="INSTALL_BASE='$PERL_BASE'"
