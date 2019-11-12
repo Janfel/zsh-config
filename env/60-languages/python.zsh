1b0d184019ec0cee8ac051e3b89d2bb834c125fd
Jan Felix Langenbach <o.hase3@gmail.com>
2019-11-12 01:43:40 +0100

    Python userbase in .local

--- env/60-languages/python.zsh
+++ env/60-languages/python.zsh
@@ -1,5 +1,9 @@
+typeset -xUT PYTHONPATH pythonpath
+
 export PYTHONSTARTUP="$XDG_CONFIG_HOME/python/init.py"
-export PYTHONUSERBASE="$XDG_DATA_HOME/python"
-export PYTHON_EGG_CACHE="$XDG_CACHE_HOME/python-eggs"
+export PYTHON_EGG_CACHE="$XDG_CACHE_HOME/python/eggs"
+export PYTHONUSERBASE="$XDG_USER_BASE"
+
+# Pylint
 export PYLINTHOME="$XDG_CACHE_HOME/pylint"
 export PYLINTRC="$XDG_CONFIG_HOME/python/pylint.conf"
