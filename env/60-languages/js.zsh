9a0d1884e457de4d1d46d51c4afccbfdbff3cb01
Jan Felix Langenbach <o.hase3@gmail.com>
2019-11-12 06:02:55 +0100

    JS improvements

--- env/60-languages/js.zsh
+++ env/60-languages/js.zsh
@@ -1,5 +1,6 @@
 export NODE_REPL_HISTORY="$XDG_DATA_HOME/nodejs/history"
 
-typeset -xUT NODE_PATH node_path
+#typeset -xUT NODE_PATH node_path
+#typeset -xUT NODE_OPTIONS node_options ' '
 
-typeset -xUT NODE_OPTIONS node_options ' '
+export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/npm.conf"
