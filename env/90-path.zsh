9f157b11b844eb6620f45bd7e75525f4470ab4c9
Jan Felix Langenbach <o.hase3@gmail.com>
2019-11-12 02:00:47 +0100

    Update PATH for .local changes.

--- env/90-path.zsh
+++ env/90-path.zsh
@@ -1,14 +1,16 @@
 typeset -U PATH path
 path=(
-    ~/.local/bin
+    ~/bin
     $path
-    ${(s.:.)"$(luarocks path --lr-bin)"}
+    $XDG_USER_BASE/bin
+    $LUAROCKS_HOME_TREE/bin
+    #${(s.:.)"$(luarocks path --lr-bin)"}
     ~/.cabal/bin
     ~/.stack/bin
     ~/.ghcup/bin
     $CARGO_HOME/bin
     $((($+commands[ruby] )) && ruby -e 'puts Gem.user_dir')/bin
-    $PERL_HOME/bin
+    $PERL_BASE/bin
     $PYTHONUSERBASE/bin
     $GOPATH/bin
     $((( $+commands[yarn] )) && yarn global bin || echo $XDG_DATA_HOME/yarn/bin)
