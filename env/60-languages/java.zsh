13bd102c1961418359a21772a17411f589e8b6af
Jan Felix Langenbach <o.hase3@gmail.com>
2019-11-12 01:41:15 +0100

    Java improvements incl. pref dir

--- env/60-languages/java.zsh
+++ env/60-languages/java.zsh
@@ -1,16 +1,30 @@
 # Placeholder
 #export JAVA_HOME="/usr/lib/jvm/default"
 #export JRE_HOME="/usr/lib/jvm/default-runtime"
-#typeset -T  _JAVA_OPTIONS java_options ' '
+
+#export JAVA_FONTS='/usr/share/fonts/TTF'
+
 typeset -xUT _JAVA_OPTIONS java_options ' '
 java_options=(
-   '-Dawt.useSystemAAFontSettings=on' 
-   '-Dswing.aatext=true'
-   '-Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
-   '-Dswing.crossplatformlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
-   '-Dsun.java2d.opengl=true'
-   '-Djdk.gtk.version=3'
-   $java_options
+    # Java config root
+    "-Djava.util.prefs.userRoot='$XDG_CONFIG_HOME/java'"
+
+    # Anti-Aliasing
+    '-Dawt.useSystemAAFontSettings=on'
+    '-Dswing.aatext=true'
+
+    # Default Look-and-Feel
+    '-Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
+    '-Dswing.crossplatformlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
+
+    # Opengl Renderer
+    '-Dsun.java2d.opengl=true'
+
+    # GTK 3
+    '-Djdk.gtk.version=3'
+
+    # Default options
+    $java_options
 )
 
 #typeset -xUT  _SILENT_JAVA_OPTIONS silent_java_options ' '
