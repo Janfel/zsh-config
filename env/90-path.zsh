30bcb631c02db6b9b4e8821f14e9b3c16f5dc0f2
Jan Felix Langenbach <o.hase3@gmail.com>
2019-11-14 04:49:42 +0100

    Make PATH treat GOPATH properly

--- env/90-path.zsh
+++ env/90-path.zsh
@@ -12,7 +12,7 @@ path=(
     $((( $+commands[ruby] )) && ruby -e 'puts Gem.user_dir')/bin
     $PERL_BASE/bin
     $PYTHONUSERBASE/bin
-    $GOPATH/bin
+    $gopath/bin
     #$((( $+commands[yarn] )) && yarn global bin || echo $XDG_DATA_HOME/yarn/bin)
     $XDG_DATA_HOME/yarn/bin
     $NPM_CONFIG_PREFIX/bin
