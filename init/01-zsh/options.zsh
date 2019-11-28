d04d43996aaba4dde990d5e00f30088a57366b5f
Jan Felix Langenbach <o.hase3@gmail.com>
2019-11-28 21:11:32 +0100

    Improve option selection

--- init/01-zsh/options.zsh
+++ init/01-zsh/options.zsh
@@ -1,8 +1,11 @@
-# Old 1
-setopt autocd
-setopt extendedglob
-setopt interactivecomments
-setopt longlistjobs
-setopt multios
-setopt promptsubst
-setopt rcexpandparam
+setopt AUTO_CD              # Use cd on directory name
+setopt EXTENDED_GLOB        # More pattern features
+setopt INTERACTIVE_COMMENTS # Comments in interactive shell
+setopt LOCAL_OPTIONS        # Allow function scoped options
+setopt LOCAL_TRAPS          # Allow function scoped traps
+setopt LOCAL_PATTERNS       # Allow disabled patterns per function
+setopt LONG_LIST_JOBS       # Longer job notifications
+setopt MULTIOS              # Allow multiple redirections
+setopt NO_BEEP              # Disable beeping
+#setopt PROMPT_SUBST         # Allow expansions in prompt string
+setopt RC_EXPAND_PARAM      # Make a{b,c}d = abd acd
