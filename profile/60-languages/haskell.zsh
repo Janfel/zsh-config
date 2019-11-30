4e95f3cf56702415673cd1095dcebda14ae9c55f
Jan Felix Langenbach <o.hase3@gmail.com>
2019-11-30 03:31:40 +0100

    Update haskell file for ghcup

--- profile/60-languages/haskell.zsh
+++ profile/60-languages/haskell.zsh
@@ -1,9 +1,12 @@
-export STACK_ROOT="$XDG_DATA_HOME/stack"
+export GHCUP_INSTALL_BASE_PREFIX=~/opt/ghcup
+
+export STACK_ROOT=$XDG_DATA_HOME/stack
 #stack_program_dirs=("$STACK_ROOT/programs/"*/*(NFOn))
 #stack_comp_tool_dirs=("$STACK_ROOT/compiler-tools/"*/*(NFOn))
 path+=(
-    #~/.cabal/bin
+    ~/.cabal/bin
     #~/.stack/bin
-    $STACK_ROOT/bin
+    #$STACK_ROOT/bin
     #~/.ghcup/bin
+    $GHCUP_INSTALL_BASE_PREFIX/.ghcup/bin
 )
