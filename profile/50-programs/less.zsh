c11d7980448bc8a29edc72a2827cc987c0407d33
Jan Felix Langenbach <o.hase3@gmail.com>
2019-11-30 02:02:17 +0100

    Add colors to less

--- profile/50-programs/less.zsh
+++ profile/50-programs/less.zsh
@@ -1,2 +1,12 @@
 export LESSHISTFILE="$XDG_CONFIG_HOME/less/history"
 export LESSKEY="$XDG_CONFIG_HOME/less/keys"
+
+# Handle color
+export LESS=-R                         # raw control chars
+export LESS_TERMCAP_mb=$'\E[1;31m'     # begin blink
+export LESS_TERMCAP_md=$'\E[1;36m'     # begin bold
+export LESS_TERMCAP_me=$'\E[0m'        # reset bold/blink
+export LESS_TERMCAP_so=$'\E[01;44;33m' # begin reverse video
+export LESS_TERMCAP_se=$'\E[0m'        # reset reverse video
+export LESS_TERMCAP_us=$'\E[1;32m'     # begin underline
+export LESS_TERMCAP_ue=$'\E[0m'        # reset underline
