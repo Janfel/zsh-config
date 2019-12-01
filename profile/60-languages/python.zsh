704e0525737cbbd50ec0d0c6f25487a741ea1993
Jan Felix Langenbach <o.hase3@gmail.com>
2019-12-01 03:50:21 +0100

    Add jupyter dirs

--- profile/60-languages/python.zsh
+++ profile/60-languages/python.zsh
@@ -9,3 +9,10 @@ export PYLINTRC="$XDG_CONFIG_HOME/python/pylint.conf"
 
 # mypy
 export MYPY_CACHE_DIR="$XDG_CACHE_HOME/mypy"
+
+# jupyter
+# https://jupyter.readthedocs.io/en/latest/projects/jupyter-directories.html
+export JUPYTER_CONFIG_DIR="$XDG_CONFIG_HOME/jupyter"
+export JUPYTER_RUNTIME_DIR="$XDG_RUNTIME_DIR/jupyter"
+#export JUPYTER_PATH
+#export JUPYTER_DATA_DIR
