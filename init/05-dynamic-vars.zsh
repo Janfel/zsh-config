76e8ca557f62046f5907a4621a0756bbc72901c3
Jan Felix Langenbach <o.hase3@gmail.com>
2020-03-16 15:56:39 +0100

    Dynamicise common env vars

--- init/05-dynamic-vars.zsh
+++
@@ -1,7 +0,0 @@
-# These are variables that can not be set once-and-for-all.
-
-if [[ -n $DISPLAY ]]; then
-	export BROWSER=/usr/bin/firefox-developer-edition
-else
-	export BROWSER=/usr/bin/links
-fi
--- profile/30-common.zsh
+++ profile/30-common.zsh
@@ -1,11 +1,7 @@
 # Common environment variables that are used by many programs.
 
-export BROWSER=/usr/bin/firefox
-export EDITOR=/usr/bin/micro
 export GIT_COMMAND=/usr/bin/git
-export PAGER=/usr/bin/less
 export SHELL=/usr/bin/zsh
-export VISUAL=/usr/bin/nvim
 
 # For shell themes.
 export DEFAULT_USER=$USER
