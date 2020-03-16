b161c0e502095d72614f42d5597984ac51fa5692
Jan Felix Langenbach <o.hase3@gmail.com>
2020-03-16 02:46:49 +0100

    Disable GTK_USE_PORTAL

--- profile/50-programs/firefox.zsh
+++ profile/50-programs/firefox.zsh
@@ -1,5 +1,7 @@
 # Mozilla Firefox
 
-# Needs xdg-desktop-portal xdg-desktop-portal-kde
-# Theming currently broken
-#export GTK_USE_PORTAL=1
+# This depends on xdg-desktop-portal and xdg-desktop-portal-kde.
+# It enables the use of KDE file dialogues, but breaks the
+# application handler for MIME types.
+# Manjaro activates this by default.
+unset GTK_USE_PORTAL
