f5f50efcd53eafea2088ec93eb7e59b2ac7fcbf7
Jan Felix Langenbach <o.hase3@gmail.com>
2020-03-16 01:14:49 +0100

    Add fpc.cfg

--- profile/60-languages/pascal.zsh
+++ profile/60-languages/pascal.zsh
@@ -1,5 +1,7 @@
 # Pascal
 
+export PPC_CONFIG_PATH="$XDG_CONFIG_HOME/pascal"
+
 # These variables are only used by my custom ptop version.
 export PTOP_CONFIG="$XDG_CONFIG_HOME/pascal/ptop.cfg"
 export PTOP_INDENT=2
