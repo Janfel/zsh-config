014d2744f1c920a5ae8200d214e628fe55b114c4
Jan Felix Langenbach <o.hase3@gmail.com>
2020-05-08 00:31:51 +0200

    Fix some perl stuff.

--- profile/60-languages/perl.zsh
+++ profile/60-languages/perl.zsh
@@ -12,14 +12,25 @@ perl5opt=(
 
 # Install packages into ~/.local.
 export PERL_BASE="$XDG_USER_BASE"
-perl5lib=($PERL_BASE $perl5lib)
+perl5lib=(
+	$PERL_BASE/lib/perl5
+	$perl5lib
+)
 perl_local_lib_root=($PERL_BASE $perl_local_lib_root)
-perl_mb_opt+="--install_base '$PERL_BASE'"
 perl_mm_opt+="INSTALL_BASE='$PERL_BASE'"
+perl_mb_opt+="--install_base '$PERL_BASE'"
 #path+=$PERL_BASE/bin
 
-# CPAN
+function perl_build_env() {
+	export PERL_MM_USE_DEFAULT=1 PERL5LIB="" \
+		PERL_AUTOINSTALL=--skipdeps          \
+		PERL_MM_OPT="INSTALLDIRS=vendor"     \
+		PERL_MB_OPT="--installdirs vendor"   \
+		MODULEBUILDRC=/dev/null
+	:
+}
 
+# CPAN
 export PERL_CPANM_HOME="$XDG_DATA_HOME/cpanminus"
 # cpan_opts=()
 # cpanm_opts=()
