34c6491c810491db0d3e06241cd6a45f3cff1efd
Jan Felix Langenbach <o.hase3@gmail.com>
2020-05-11 21:08:35 +0200

    Move dotctl repo check to runtime

--- init/80-features/dotctl.zsh
+++ init/80-features/dotctl.zsh
@@ -1,4 +1,5 @@
 # Dotfile management using git bare-repos.
-###if [[ -d $DOTFILES_REPO ]]
 
-alias dotctl="git --git-dir=$DOTFILES_REPO --work-tree=$HOME"
+if [[ -d $DOTFILES_REPO ]]; then
+	alias dotctl="git --git-dir=$DOTFILES_REPO --work-tree=$HOME"
+fi
