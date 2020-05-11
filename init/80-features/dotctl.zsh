de8feddcbf895d3e8fc50fae0fada8f07251ff82
Jan Felix Langenbach <o.hase3@gmail.com>
2020-05-11 21:27:29 +0200

    Remove dotctl check

--- init/80-features/dotctl.zsh
+++ init/80-features/dotctl.zsh
@@ -1,5 +1,3 @@
 # Dotfile management using git bare-repos.
 
-if [[ -d $DOTFILES_REPO ]]; then
-	alias dotctl="git --git-dir=$DOTFILES_REPO --work-tree=$HOME"
-fi
+alias dotctl="git --git-dir=$DOTFILES_REPO --work-tree=$HOME"
