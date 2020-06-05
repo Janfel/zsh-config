fafe0b3b155bedbdec731b2f3e5e17929d4c732a
Jan Felix Langenbach <o.hase3@gmail.com>
2020-06-05 15:48:53 +0200

    Add “antibody path” completion.

--- init/50-programs/antibody.zsh
+++ init/50-programs/antibody.zsh
@@ -2,6 +2,6 @@
 ###if (($+commands[antibody]))
 
 function _antibody {
-	reply=(help bundle update home purge list init)
+	reply=(help bundle update home purge list path init)
 }
 compctl -K _antibody antibody
