205d9dcc7f417aa71f33b1a0fd5cf3c4c83aa0e4
Jan Felix Langenbach <o.hase3@gmail.com>
2020-06-10 15:42:12 +0200

    Fix tabwidth on instant prompt.

---
+++ init/02-tty.zsh
@@ -0,0 +1,4 @@
+# TTY
+
+# Set terminal to output tabs (default).
+stty tabs
--- init/80-features/tabwidth.zsh
+++ init/80-features/tabwidth.zsh
@@ -1,7 +1,5 @@
 # Configure how tabs are displayed.
 ###if (($+commands[tabs]))
 
-# Set terminal to output tabs (default).
-stty tabs
 # Set tab width to four.
 tabs -4
