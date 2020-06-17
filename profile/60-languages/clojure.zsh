9abdb714a14850d2cea7aa2de69c41964f45de9f
Jan Felix Langenbach <o.hase3@gmail.com>
2020-06-18 00:52:53 +0200

    Configure Leiningen and Boot dirs for Clojure.

---
+++ profile/60-languages/clojure.zsh
@@ -0,0 +1,11 @@
+# Clojure
+
+## Leiningen
+###if (($+commands[lein]))
+export LEIN_HOME="$XDG_CONFIG_HOME/lein"
+###endif
+
+## Boot
+###if (($+commands[boot]))
+export BOOT_HOME="$XDG_CONFIG_HOME/boot"
+###endif
