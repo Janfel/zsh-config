7f86cf4bd71ce2fa8306508a4027e8eb41ca87f0
Jan Felix Langenbach <o.hase3@gmail.com>
2020-06-29 17:06:23 +0200

    Fix avg function.

--- init/80-features/math.zsh
+++ init/80-features/math.zsh
@@ -2,7 +2,7 @@
 
 function average() {
 	: 'Calculate the integer average of $* or stdin.'
-	local nums+=($*)
+	local nums=($*)
 	(($#nums)) || nums+=($(</dev/stdin))
 	local dc_prog='? zsn [+ z 2 <c] dsc x ln / p'
 	printf '%s ' $nums | dc -e $dc_prog
