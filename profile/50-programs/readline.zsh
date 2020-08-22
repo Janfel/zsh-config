b8b0df8d95cbbb2909a85da0030adf30f6ef68af
Jan Felix Langenbach <o.hase3@gmail.com>
2020-08-22 17:12:26 +0200

    Move .inputrc into XDG.

--- profile/50-programs/readline.zsh
+++ profile/50-programs/readline.zsh
@@ -1,4 +1,7 @@
 # GNU Readline
 
+# Put input configuration into XDG directory.
+export INPUTRC="$XDG_CONFIG_HOME/readline/inputrc"
+
 # Put command histories into XDG directory.
 export RLWRAP_HOME="$XDG_DATA_HOME/readline"
