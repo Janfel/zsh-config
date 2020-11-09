bc715b3f6eb10c0a4537310f75659be5969ec5ca
Jan Felix Langenbach <o.hase3@gmail.com>
2020-11-09 03:44:53 +0100

    Change Lua Path/CPath generation.

--- env/60-languages/lua.zsh
+++
@@ -1,7 +0,0 @@
-# Lua
-
-# Lua uses the semicolon as the path separator.
-typeset -xUT LUA_PATH      lua_path      ';'
-typeset -xUT LUA_CPATH     lua_cpath     ';'
-typeset -xUT LUA_PATH_5_3  lua_path_5_3  ';'
-typeset -xUT LUA_CPATH_5_3 lua_cpath_5_3 ';'
