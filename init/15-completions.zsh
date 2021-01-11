980535a4110f8cef2c2ce902eb3c76828ef83966
Jan Felix Langenbach <o.hase3@gmail.com>
2021-01-11 18:53:36 +0100

    Add dotnet shell completion.

--- init/15-completions.zsh
+++ init/15-completions.zsh
@@ -3,3 +3,14 @@
 ###if (($+commands[rederr]))
 compctl _command rederr
 ###endif
+
+###if (($+commands[dotnet]))
+_dotnet_zsh_complete()
+{
+  local completions=("$(dotnet complete "$words")")
+
+  reply=( "${(ps:\n:)completions}" )
+}
+
+compctl -K _dotnet_zsh_complete dotnet
+###endif
