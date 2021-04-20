736c5a7f0ff6cd8f0a52500adc76c24ff0a2c7f1
Jan Felix Langenbach <o.hase3@gmail.com>
2021-04-20 04:12:25 +0200

    Clean up some Chicken Scheme code.

--- env/60-languages/scheme.zsh
+++ env/60-languages/scheme.zsh
@@ -1,9 +1,10 @@
 # Scheme
 
 # Chicken Scheme
-typeset -xUT CHICKEN_INCLUDE_PATH chicken_include_path
-# typeset -xUT CHICKEN_OPTIONS chicken_options
+typeset -xUT CHICKEN_INCLUDE_PATH    chicken_include_path
 typeset -xUT CHICKEN_REPOSITORY_PATH chicken_repository_path
+#typeset -xUT CHICKEN_C_INCLUDE_PATH  chicken_c_include_path
+#typeset -xUT CHICKEN_C_LIBRARY_PATH  chicken_c_library_path
 
 # Guile Scheme
 #typeset -xUT GUILE_LOAD_PATH guile_load_path
--- profile/60-languages/scheme.zsh
+++ profile/60-languages/scheme.zsh
@@ -3,18 +3,16 @@
 # Chicken Scheme
 
 export CHICKEN_INSTALL_REPOSITORY="$XDG_USER_BASE/lib/chicken/5"
-chicken_repository_path=(
-	/usr/lib/chicken/*(NF)
-	$CHICKEN_INSTALL_REPOSITORY
-)
-chicken_include_path=(
-	$chicken_repository_path
-)
 
+chicken_repository_path=(/usr/lib/chicken/*(NF) $CHICKEN_INSTALL_REPOSITORY)
+chicken_include_path=($chicken_repository_path)
+
+# These are undocumented variables that are passed to the C compiler.
 # CHICKEN_C_INCLUDE_PATH
 # CHICKEN_C_LIBRARY_PATH
 
-# CHICKEN_DOC_REPOSITORY='/usr/local/share/doc/chicken'
+# Where the chicken-doc documentation can be found.
+CHICKEN_DOC_REPOSITORY='/usr/local/share/doc/chicken'
 
 # Guile Scheme
 
