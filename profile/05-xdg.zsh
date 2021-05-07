2a61d7c73ea0803873cb8341f24b1933e1ff5292
Jan Felix Langenbach <o.hase3@gmail.com>
2021-05-07 03:31:46 +0200

    Introduce $XDG_BIN_HOME.

--- profile/05-xdg.zsh
+++ profile/05-xdg.zsh
@@ -3,9 +3,14 @@
 # This is a custom variable following the XDG naming scheme.
 # It defines the prefix used for the user-local file system hierarchy.
 export XDG_USER_BASE="$HOME/.local"
+
+# This is a custom variable that is used by ‘ghcup’.
+# It defines where to install local binaries.
+export XDG_BIN_HOME="$XDG_USER_BASE/bin"
+
 # Remove this entry from PATH so it doesn’t appear sooner.
-path=(${path:#"$XDG_USER_BASE/bin"})
-path+=$XDG_USER_BASE/bin
+path=(${path:#"$XDG_BIN_HOME"})
+path+="$XDG_BIN_HOME"
 
 # The XDG variables are defined in terms of $XDG_USER_BASE.
 
