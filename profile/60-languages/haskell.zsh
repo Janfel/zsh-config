addde1c8e90ec2d7c24d834ce82a248fe4746067
Jan Felix Langenbach <o.hase3@gmail.com>
2021-05-07 03:35:26 +0200

    Change ghcup to use $GHCUP_USE_XDG_DIRS.

--- profile/60-languages/haskell.zsh
+++ profile/60-languages/haskell.zsh
@@ -1,7 +1,11 @@
 # Haskell
 
-# Where GHCUP installs GHC.
-export GHCUP_INSTALL_BASE_PREFIX="$XDG_USER_OPT/ghcup"
+# Finally we can just set this and be happy.
+# Before this was implemented, we had to fudge around with
+# ‘$GHCUP_INSTALL_BASE_PREFIX’ and a ‘./.ghcup/ -> ./’ symlink.
+# See https://gitlab.haskell.org/haskell/ghcup-hs/-/blob/master/lib/GHCup/Utils/Dirs.hs
+# for the relevant code.
+export GHCUP_USE_XDG_DIRS=1
 
 # Cabal
 export CABAL_DIR="$XDG_DATA_HOME/cabal" # Not documented as of 2020-02-15.
@@ -17,6 +21,4 @@ path+=(
 	$CABAL_DIR/bin
 	#~/.stack/bin
 	#$STACK_ROOT/bin
-	#~/.ghcup/bin
-	$(realpath -q $GHCUP_INSTALL_BASE_PREFIX/.ghcup/bin)
 )
