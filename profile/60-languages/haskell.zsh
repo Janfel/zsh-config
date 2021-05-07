6f84f70f3881bc61a7fb04e5d17fd190a574ae79
Jan Felix Langenbach <o.hase3@gmail.com>
2021-05-07 03:39:37 +0200

    Simplify Haskell $PATH code.

--- profile/60-languages/haskell.zsh
+++ profile/60-languages/haskell.zsh
@@ -5,20 +5,17 @@
 # ‘$GHCUP_INSTALL_BASE_PREFIX’ and a ‘./.ghcup/ -> ./’ symlink.
 # See https://gitlab.haskell.org/haskell/ghcup-hs/-/blob/master/lib/GHCup/Utils/Dirs.hs
 # for the relevant code.
+# Notably binaries are now installed to ‘${XDG_BIN_HOME-"$HOME/.local/bin"}’
+# which is a nonstandard location.
 export GHCUP_USE_XDG_DIRS=1
 
 # Cabal
 export CABAL_DIR="$XDG_DATA_HOME/cabal" # Not documented as of 2020-02-15.
 export CABAL_CONFIG="$XDG_CONFIG_HOME/cabal/config"
+path+="$CABAL_DIR/bin"
 
 # Stack
 export STACK_ROOT=$XDG_DATA_HOME/stack
 #stack_program_dirs=("$STACK_ROOT/programs/"*/*(NFOn))
 #stack_comp_tool_dirs=("$STACK_ROOT/compiler-tools/"*/*(NFOn))
-
-path+=(
-	#~/.cabal/bin
-	$CABAL_DIR/bin
-	#~/.stack/bin
-	#$STACK_ROOT/bin
-)
+#path+="$STACK_ROOT/bin"
