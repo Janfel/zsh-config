f1cc59f58293138f09d043f526d376b11af37f4d
Jan Felix Langenbach <o.hase3@gmail.com>
2022-02-17 02:11:01 +0100

    Add xml-exredump helper function.

--- init/50-programs/xmlstarlet.zsh
+++ init/50-programs/xmlstarlet.zsh
@@ -2,9 +2,18 @@
 ###if (($+commands[xml]))
 
 function xml-exmime() {
-	: 'Extract a mime-type element from a MIME XML document.'
+	: 'Extract a <mime-type> element from a MIME XML document.'
 	xml select --template \
 		--match "//_:mime-type[@type='$1']" \
 		--copy-of . \
 		--nl ${@[2,-1]}
+}
+
+function xml-exredump() {
+	: 'Extract all <game> elements with a given hash of a given type
+	from a redump.org data file.'
+	xml select --template \
+		--match "//game/rom[@$1='$2']" \
+		--copy-of '..' \
+		--nl ${@[3,-1]}
 }
