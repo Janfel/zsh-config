acee3ee1344cb0a21f8c3292c195dc59076b6c2c
Jan Felix Langenbach <insert-penguin@protonmail.com>
2023-05-11 04:37:17 +0200

    Add note to research $DOTNET_CLI_HOME

--- profile/60-languages/dotnet.zsh
+++ profile/60-languages/dotnet.zsh
@@ -6,6 +6,11 @@ export DOTNET_CLI_TELEMETRY_OPTOUT=1
 # Explicitly set root of installation.
 export DOTNET_ROOT='/opt/dotnet'
 
+# TODO: Figure out more about $DOTNET_CLI_HOME.
+#
+# export DOTNET_CLI_HOME="$XDG_DATA_HOME/dotnet"
+# export _DOTNET_HOME="$DOTNET_CLI_HOME/.dotnet"
+
 export NUGET_PACKAGES="$XDG_DATA_HOME/NuGet/packages"
 
 # Unofficial, change PATH when configurable.
