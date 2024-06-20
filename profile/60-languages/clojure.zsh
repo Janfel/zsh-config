cf338011d845ab4423d0ff4968db0c871178aacc
Jan Felix Langenbach <insert-penguin@protonmail.com>
2024-06-21 00:37:39 +0200

    Move leiningen directory to .config/leiningen
    This follows the behavior introduced in 2.11.0.

--- profile/60-languages/clojure.zsh
+++ profile/60-languages/clojure.zsh
@@ -2,7 +2,11 @@
 
 ## Leiningen
 ###if (($+commands[lein]))
-export LEIN_HOME="$XDG_CONFIG_HOME/lein"
+
+# Remove this once 2.10.1 comes out
+# https://github.com/technomancy/leiningen/issues/2087
+export LEIN_HOME="$XDG_CONFIG_HOME/leiningen"
+
 ###endif
 
 ## Boot
