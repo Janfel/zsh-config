8b3707061076a89623e04a58469d12a8f90e6437
Jan Felix Langenbach <insert-penguin@protonmail.com>
2024-06-21 01:04:16 +0200

    Leave GTK_USE_PORTAL alone
    This was a workaround for a broken setting by Manjaro. This setting has
    since been removed.

--- profile/50-programs/firefox.zsh
+++
@@ -1,7 +0,0 @@
-# Mozilla Firefox
-
-# This depends on xdg-desktop-portal and xdg-desktop-portal-kde.
-# It enables the use of KDE file dialogues, but breaks the
-# application handler for MIME types.
-# Manjaro activates this by default.
-unset GTK_USE_PORTAL
