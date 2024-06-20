1c6c6a6b7dda5fc67ae7f938523c72cee1c6262e
Jan Felix Langenbach <insert-penguin@protonmail.com>
2024-06-21 00:31:49 +0200

    Create history directory if necessary

--- init/01-zsh/history.zsh
+++ init/01-zsh/history.zsh
@@ -3,3 +3,7 @@
 HISTFILE="$XDG_DATA_HOME/zsh/history"
 HISTSIZE=50000
 SAVEHIST=40000
+
+# Create history directory if necessary.
+# If this doesn’t exist, Zsh won’t save any history at all.
+mkdir -p ${HISTFILE:h}
