283b464326d9470287546a05025635245da1ebcf
Jan Felix Langenbach <insert-penguin@protonmail.com>
2024-07-07 00:52:55 +0200

    Move setting of tab width to 02-tty
    Now it won’t mess up p10k-instant-prompt anymore.

--- init/02-tty.zsh
+++ init/02-tty.zsh
@@ -1,4 +1,11 @@
 # TTY
 
+# Configure how tabs are displayed.
+
 # Set terminal to output tabs (default).
 stty tabs
+
+###if (($+commands[tabs]))
+# Set tab width to four.
+tabs -4
+###endif
--- init/80-features/tabwidth.zsh
+++
@@ -1,5 +0,0 @@
-# Configure how tabs are displayed.
-###if (($+commands[tabs]))
-
-# Set tab width to four.
-tabs -4
