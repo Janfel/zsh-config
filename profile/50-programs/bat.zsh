119cc1f4183cc6564bb56a6d2537021bc9556e03
Jan Felix Langenbach <insert-penguin@protonmail.com>
2024-07-06 03:22:24 +0200

    Implement colored man pages in it’s own module

--- profile/50-programs/bat.zsh
+++ profile/50-programs/bat.zsh
@@ -2,5 +2,6 @@
 
 # Pager configuration.
 export BAT_PAGER='less --raw-control-chars --quit-if-one-screen'
+
 # OneDark theme.
 export BAT_THEME='OneHalfDark'
--- profile/50-programs/less.zsh
+++ profile/50-programs/less.zsh
@@ -1,14 +1,14 @@
 # Less
 
-export LESSHISTFILE="$XDG_CONFIG_HOME/less/history"
-export LESSKEY="$XDG_CONFIG_HOME/less/keys"
+# Pass through color escape sequences.
+export LESS='-R'
 
-# Color handling.
-export LESS=-R                         # raw control chars
-export LESS_TERMCAP_mb=$'\E[1;31m'     # begin blink
-export LESS_TERMCAP_md=$'\E[1;36m'     # begin bold
-export LESS_TERMCAP_me=$'\E[0m'        # reset bold/blink
-export LESS_TERMCAP_so=$'\E[01;44;33m' # begin reverse video
-export LESS_TERMCAP_se=$'\E[0m'        # reset reverse video
-export LESS_TERMCAP_us=$'\E[1;32m'     # begin underline
-export LESS_TERMCAP_ue=$'\E[0m'        # reset underline
+###if [[ -f "$XDG_CONFIG_HOME/less/history" ]]
+###warn Please move your LESSHISTFILE to $XDG_STATE_HOME/lesshst
+export LESSHISTFILE="$XDG_CONFIG_HOME/less/history"
+###endif
+
+###if [[ -f "$XDG_CONFIG_HOME/less/keys" ]]
+###warn Please move your LESSKEY to $XDG_CONFIG_HOME/lesskey
+export LESSKEY="$XDG_CONFIG_HOME/less/keys"
+###endif
