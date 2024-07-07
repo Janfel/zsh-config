a8b13dc9a5fd02c671c8b2dacd2341e012eb0072
Jan Felix Langenbach <insert-penguin@protonmail.com>
2024-07-07 20:09:51 +0200

    Move fzf integration to 40-site-plugins

---
+++ init/40-site-plugins/fzf.zsh
@@ -0,0 +1,9 @@
+# FZF Integration
+
+###if [[ -f /usr/share/fzf/completion.zsh ]]
+source /usr/share/fzf/completion.zsh
+###endif
+
+###if [[ -f /usr/share/fzf/key-bindings.zsh ]]
+source /usr/share/fzf/key-bindings.zsh
+###endif
