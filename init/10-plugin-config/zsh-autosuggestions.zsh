7e510695c36e82633acda558d663f57405deff6b
Jan Felix Langenbach <insert-penguin@protonmail.com>
2024-07-10 05:06:00 +0200

    Set upper limits for zsh-autosuggestions

---
+++ init/10-plugin-config/zsh-autosuggestions.zsh
@@ -0,0 +1,13 @@
+# Zsh Autosuggestions
+
+# See: https://github.com/zsh-users/zsh-autosuggestions
+
+# TODO: Change this when running on a 8 color terminal like the Linux console.
+# typeset -g ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE
+
+# Disable autosuggestions when the buffer exceeds this size.
+# The recommended value is 20.
+typeset -g ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=50
+
+# Ignore history elements longer than 50 characters.
+typeset -g ZSH_AUTOSUGGEST_HISTORY_IGNORE='?(#c50,)'
