dca6787839ecbf54a62df6f077b74d5774642c31
Jan Felix Langenbach <insert-penguin@protonmail.com>
2024-07-10 02:50:13 +0200

    Disable most Java env vars
    Most of these were seemingly unnecessary (Font AA) or actively broke
    things (OpenGL Renderer, JAVA_HOME (?)).

--- profile/60-languages/java.zsh
+++ profile/60-languages/java.zsh
@@ -4,8 +4,13 @@
 # Applications are all programs started using the ‘java’ command.
 # Tools are the programs shipped with the JDK, like ‘javac’ and ‘jshell’.
 
-export JAVA_HOME='/usr/lib/jvm/default'
-export JRE_HOME='/usr/lib/jvm/default-runtime'
+# Setting these variables breaks scripts that prepend a specific version of
+# Java onto ‘PATH’, such as the PKGBUILD for PrismLauncher.
+# export JAVA_HOME='/usr/lib/jvm/default'
+# export JRE_HOME='/usr/lib/jvm/default-runtime'
+
+# Tell Java where to find the system TrueType fonts.
+# Is this still necessary on a non-bare-bones system?
 export JAVA_FONTS='/usr/share/fonts/TTF'
 
 # List of Look-and-Feel classes:
@@ -15,42 +20,38 @@ export JAVA_FONTS='/usr/share/fonts/TTF'
 # com.sun.java.swing.plaf.motif.MotifLookAndFeel
 # com.sun.java.swing.plaf.windows.WindowsLookAndFeel
 
+# JAVA_TOOL_OPTIONS
 # Affects applications and tools.
 # The documented replacement for _JAVA_OPTIONS.
-java_tool_options=(
-	$java_tool_options
 
-	# Java config root
-	# "-Djava.util.prefs.userRoot='$XDG_CONFIG_HOME/java'"
+# Java config root
+# java_tool_options+="-Djava.util.prefs.userRoot='$XDG_CONFIG_HOME/java'"
 
-	# GTK Look-and-Feel (looks very bad when using dark mode)
-	# '-Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
-	# '-Dswing.crossplatformlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
+# GTK Look-and-Feel (looks very bad when using dark mode)
+# java_tool_options+='-Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
+# java_tool_options+='-Dswing.crossplatformlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
 
-	# OpenGL Renderer
-	'-Dsun.java2d.opengl=true'
+# GTK Version
+# Used by JavaFX, the GTK-LaF and other components.
+# Java 9+ defaults to GTK3 (?), while Java 8 defaults to GTK2.
+'-Djdk.gtk.version=3'
 
-	# GTK Version
-	# Java 9+ defaults to GTK3, while Java 8 defaults to GTK2.
-	'-Djdk.gtk.version=3'
-)
+# JDK_JAVA_OPTIONS
+# Affects only applications started using the ‘java’ command.
+# Supported since Java 9, will be ignored by Java 8.
+
+# OpenGL Renderer
+# This breaks the rendering of multiple GUI applications,
+# which is why it is not enabled by default.
+# jdk_java_options+='-Dsun.java2d.opengl=true'
 
 # Font Anti-Aliasing
 # See https://wiki.archlinux.org/index.php/Java_Runtime_Environment_fonts for
 # a list of possible values.
-
-###if   [[ $HOST == PhantomJFL ]]
-java_tool_options+=('-Dawt.useSystemAAFontSettings=lcd')
-###elif [[ $HOST == TeslaJFL   ]]
-java_tool_options+=('-Dawt.useSystemAAFontSettings=gasp')
-###else
-java_tool_options+=('-Dawt.useSystemAAFontSettings=gasp')
-###endif
-
-# Affects only applications started using the ‘java’ command.
-# Supported since Java 9, will be ignored by Java 8.
-#jdk_java_options=($jdk_java_options)
-
+# This shouldn’t be necessary on a system that runs an integrated
+# ‘xsettingsd’, like Plasma with ‘kde-gtk-config’ or GNOME.
+# jdk_java_options+='-Dawt.useSystemAAFontSettings=gasp'
+# jdk_java_options+='-Dswing.aatext=true'
 
 # Gradle
 
